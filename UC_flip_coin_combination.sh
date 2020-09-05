e1490387494a9e76eb8538457e8cff99847d2c5a
codexakshay <codexakshay@gmail.com>
2020-09-05 20:40:08 +0530

    UC_flipCoinCombination

--- UC_flip_coin_combination.sh
+++ UC_flip_coin_combination.sh
@@ -1,6 +1,19 @@
-flip=$(($(($RANDOM%10))%2))
-if [ $flip -eq 1 ]; then
-    echo "HEADS"
+declare -A dict
+h_c=0
+l_c=0
+h=1
+for ((i=1;i<=20;i++))
+do
+flip=`shuf -i 0-1 -n 1`
+if [ $flip -eq $h ]; then
+        h_c=$(($h_c+1))
+        dict[H]=$h_c
 else
-    echo "TAILS"
+        l_c=$(($l_c+1))
+        dict[T]=$l_c
 fi
+done
+echo
+echo "H : "${dict[H]}
+echo "T : "${dict[T]}
+
